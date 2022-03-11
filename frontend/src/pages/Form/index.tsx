c7c1a143cf8c93217904320c02813445751a5d44
Adriana Hinsching Leite <adriana.leite@iob.com.br>
2022-03-10 23:00:45 -0300

    Navegação dos botões

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,4 +1,6 @@
 import './styles.css';
+import { Link } from 'react-router-dom';
+
 
 function Form() {
 
@@ -31,10 +33,13 @@ function Form() {
                         </select>
                     </div>
                     <div className="dsmovie-form-btn-container">
-                        <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+                            <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
                 </form >
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
+
             </div >
         </div >
     );
